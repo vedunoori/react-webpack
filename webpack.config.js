8ff6ae249bcdeb49a05b540c0fd135cb7a45c46c
vedunoori sridhar <sridhar.v@shyftinnovations.com>
2022-12-21 15:55:25 +0530

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: './index.js',
+   output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js'
+   },
+   devServer: {
+      port: 8001
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+         }
+              ]
+   },
+   plugins:[
+      new HtmlWebpackPlugin({
+         template: './public/index.html'
+      })
+   ]
+}
